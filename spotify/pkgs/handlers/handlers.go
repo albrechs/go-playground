2bce8fe3c35a182be109211801c2364158c46b6b
Trevor <trevor@sta-mbp.localdomain>
2022-07-06 13:33:50 -0700

    adding comment to note client secret rotation

--- spotify/pkgs/handlers/handlers.go
+++ spotify/pkgs/handlers/handlers.go
@@ -5,7 +5,7 @@ import (
 )
 
 const clientid = "58f7888547c24f058ece41fed973bf37"     // Your client id
-const clientsecret = "8405db97ae294a9885f064855b5fae79" // Your secret
+const clientsecret = "8405db97ae294a9885f064855b5fae79" // client secret has been reset
 const statekey = "spotify_auth_state"
 
 var redirecturi = "http://localhost:8888/callback" // Your redirect uri

0e8ff968b969f40d693626b9b3aa1f41c51b74f0
Trevor <trevor@sta-mbp.localdomain>
2022-07-06 13:36:36 -0700

    sourcing spotify client secret from env

--- spotify/pkgs/handlers/handlers.go
+++ spotify/pkgs/handlers/handlers.go
@@ -2,11 +2,12 @@ package handlers
 
 import (
 	"html/template"
+	"os"
 )
 
-const clientid = "58f7888547c24f058ece41fed973bf37"     // Your client id
-const clientsecret = "8405db97ae294a9885f064855b5fae79" // client secret has been reset
+const clientid = "58f7888547c24f058ece41fed973bf37" // Your client id
 const statekey = "spotify_auth_state"
 
+var clientsecret = os.Getenv("SPOTIFY_CLIENT_SECRET")
 var redirecturi = "http://localhost:8888/callback" // Your redirect uri
 var templates = template.Must(template.ParseFiles("tmpl/top-tracks.html"))
